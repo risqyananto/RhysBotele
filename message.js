b702ef88ff157ff0753a14bfc520fe47f3711456
risqyananto <risqyananto@outlook.com>
2025-10-27 01:44:18 +0700

    Update message.js

--- message.js
+++ message.js
@@ -6,10 +6,13 @@ return ctx.reply(`
       Need more help? Come join our *guild*  
       
       📌 *Rankings*  
-      \`top\` \`my\`
+      \`/top\` \`/my\`
       
       💰 *Economy*  
-      \`rhystal\` \`give\` \`daily\` \`pay\` \`quest\`
+       \`/store\` \`/rhystal\` \`/give\` \`/daily\` \`/pay\` \`/referral\`  
+
+      😆 *Fun*  
+      \`/gacha\` \`/inventory\`  \`/shop(coming soon)\`  \`/hunt(coming soon)\`  
       `
       /*🌱 *Animals*  
       \`zoo\` \`hunt\` \`sell\` \`sacrifice\` \`battle\` \`inv\` \`equip\` \`authowent\` \`owodex\` \`lootbox\`  
@@ -26,25 +29,22 @@ return ctx.reply(`
 const menuAdmin = (ctx) => {
 return ctx.reply(`
       🔹 *Command List*
+         🔹 *Command List*
       Here is the list of commands\!
       For more info on a specific command, use \`/help {command}\`\.
       Need more help? Come join our *guild*  
       
       📌 *Rankings*  
-      \`top\` \`my\`
+      \`/top\` \`/my\`
       
       💰 *Economy*  
-      \`cowoncy\` \`give\` \`daily\` \`vote\` \`quest\` \`checklist\` \`shop\` \`buy\`
-      
-      🌱 *Animals*  
-      \`zoo\` \`hunt\` \`sell\` \`sacrifice\` \`battle\` \`inv\` \`equip\` \`authowent\` \`owodex\` \`lootbox\`  
-      \`crate\` \`battlesetting\` \`team\` \`weapon\` \`rename\` \`dismantle\`
-      
-      🎲 *Gambling*  
-      \`slots\` \`coinflip\` \`lottery\` \`blackjack\`
-      
+       \`/store\` \`/rhystal\` \`/give\` \`/daily\` \`/pay\` \`/referral\`  
+
       😆 *Fun*  
-      \`8b\` \`define\` \`gif\` \`pic\` \`translate\` \`roll\` \`choose\` \`bell\`
+      \`/gacha\` \`/inventory\`  \`/shop(coming soon)\`  \`/hunt(coming soon)\`
+      
+      🛠️ *Admin*
+      \`/addadmin\` \`/deleteadmin\` \`/maketicket\` \`/makerhystal\` \`/ban\`
         `, { parse_mode: 'Markdown' });
 }
 module.exports = { menuAdmin, menuMember}
